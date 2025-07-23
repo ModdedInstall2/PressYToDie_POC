7bb4ba2dfbb2442f3e9485a5d02a731db926d8d0
Takashi <takashi.salv@proton.me>
2025-07-23 15:46:11 -0700

    Removed the huge recording file

--- player.gd
+++ player.gd
@@ -9,27 +9,37 @@ var dead = false
 
 func _physics_process(delta: float) -> void:
 	var sprite = get_node("AnimatedSprite2D")
-	var rc = get_node("CollisionShape2D/Stuck?")
-	
 	
 	if not dead:
 		motion_mode = CharacterBody2D.MOTION_MODE_GROUNDED
 		collision_mask = 1
 		
-		print(rc.get_collider())
-		if rc.get_collider() == $level/white:
-			dead = true
-		
 		if not is_on_floor():
 			velocity += get_gravity() * delta
+		
+		if is_on_floor():
+			rotation = get_floor_normal().angle() + PI/2
+		else:
+			rotation = 0
 		
 		if is_on_floor():
 			doubleJump = 0
 			if sprite.animation == &"jump":
 				sprite.play(&"idle")
+			
+			set_floor_snap_length(5)
+			set_floor_max_angle(1.5)
+		elif !is_on_floor():
+			set_floor_snap_length(1)
+			set_floor_max_angle(0.785398)
+		apply_floor_snap()
 		
 		if Input.is_action_just_pressed("a") and (is_on_floor() or doubleJump == 0):
 			velocity.y = JUMP_VELOCITY
+			if get_floor_normal().x < 0 and get_floor_normal().y != -1:
+				velocity.x = SPEED * get_floor_normal().x * 2
+			elif get_floor_normal().x > 0 and get_floor_normal().y != -1:
+				velocity.x = -SPEED * get_floor_normal().x * 2
 			if not is_on_floor():
 				doubleJump = 1
 			for i in range(3):
@@ -63,7 +73,6 @@ func _physics_process(delta: float) -> void:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			if sprite.animation == &"walk":
 				sprite.stop()
-
 		move_and_slide()
 		if is_on_floor():
 			dash = 0
@@ -88,34 +97,18 @@ func _physics_process(delta: float) -> void:
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			velocity.y = move_toward(velocity.y, 0, SPEED)
-		
-		move_and_slide()
 	
 	if Input.is_action_just_pressed("y"):
 		await get_tree().create_timer(0.05).timeout
 		if not dead:
 			dead = true
-			$Timer.start()
+			$Timer.start(10.0)
 		else:
 			$Timer.start(0.05)
-	
-	# My attempt at hacking together slope rotation.
-	#if leftRC.is_colliding() and rightRC.is_colliding() and is_on_floor():
-	#	print(leftRC.get_collision_point())
-	#	print(rightRC.get_collision_point())
-	#	if leftRC.get_collision_point().y > rightRC.get_collision_point().y and slope == 0:
-	#		while rotation_degrees > -45.0:
-	#			rotation -= 0.05
-	#		rotation_degrees = -45.0
-	#		slope = 1
-	#	elif rightRC.get_collision_point().y > leftRC.get_collision_point().y and slope == 0:
-	#		while rotation_degrees < 45.0:
-	#			rotation += 0.05
-	#		rotation_degrees = 45.0
-	#		slope = 1
-	#if not direction:
-	#	rotation = 0
-	#	slope = 0
 
 func _on_timer_timeout() -> void:
 	dead = false
+	var currentPos = position
+	position.x += 1
+	if currentPos == position:
+		Input.action_press("y")
