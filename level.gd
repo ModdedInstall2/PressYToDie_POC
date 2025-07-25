07de2fd8d845f0d67ff5d533cf0d079eacdef749
Takashi <takashi.salv@proton.me>
2025-07-25 12:48:28 -0700

    Buttons can be activated now

--- level.gd
+++ level.gd
@@ -1,17 +1,22 @@
 extends Node2D
 
-var btn1 := false
+var permaButtons := [0]
+var permaCells := [Vector2i(12, 6)]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$btns/button.set_cell(Vector2i(12, 6), 0, Vector2i(2, 2))
+	for i in range(len(permaButtons)):
+		$btns/button.set_cell(permaCells[i], 0, Vector2i(2, 2))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if $"btns/permaTriggers/1".get_overlapping_bodies() != []:
-		if btn1 == false:
-			$btns/button.set_cell(Vector2i(12, 6), 0, Vector2i(1, 3))
-			btn1 = true
-			await get_tree().create_timer(0.5).timeout
-			$btns/button.set_cell(Vector2i(12, 6), 0, Vector2i(2, 3))
+	for i in range(len(permaButtons)):
+		var currentBtn := get_node("btns/permaTriggers/" + str(i))
+		if currentBtn.get_overlapping_bodies() != []:
+			if permaButtons[i] == 0:
+				$btns/button.set_cell(permaCells[i], 0, Vector2i(1, 3))
+				permaButtons[i] = 1
+				await get_tree().create_timer(0.5).timeout
+				$btns/button.set_cell(permaCells[i], 0, Vector2i(2, 3))
