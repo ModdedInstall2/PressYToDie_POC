a72c70235ab775eb4b3321dd80cd899220682208
Takashi <takashi.salv@proton.me>
2025-07-25 13:19:19 -0700

    Permabuttons work now (hardcoded). Also did a tileset edit.

--- level.gd
+++ level.gd
@@ -2,21 +2,41 @@ extends Node2D
 
 var permaButtons := [0]
 var permaCells := [Vector2i(12, 6)]
-
+var permaLights := [Vector2i(13, 4)]
+var permaDoors := [[Vector2i(13, 5), Vector2i(13, 6)]]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for i in range(len(permaButtons)):
 		$btns/button.set_cell(permaCells[i], 0, Vector2i(2, 2))
-
+		$btns/button.set_cell(permaLights[i], 0, Vector2i(2, 1))
+		var door := Array(permaDoors[i])
+		$door.set_cell(door[0], 0, Vector2i(1, 2))
+		$door.set_cell(door[1], 0, Vector2i(0, 2))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	for i in range(len(permaButtons)):
 		var currentBtn := get_node("btns/permaTriggers/" + str(i))
+		var door := Array(permaDoors[i])
 		if currentBtn.get_overlapping_bodies() != []:
 			if permaButtons[i] == 0:
 				$btns/button.set_cell(permaCells[i], 0, Vector2i(1, 3))
+				$btns/button.set_cell(permaLights[i], 0, Vector2i(1, 1))
+				$door.set_cell(door[0], 0, Vector2i(3, 0))
+				$door.set_cell(door[1], 0, Vector2i(3, 1))
+				permaButtons[i] = 1
+				await get_tree().create_timer(0.5).timeout
+				$btns/button.set_cell(permaCells[i], 0, Vector2i(2, 3))
+	for i in range(len(permaButtons)):
+		var currentBtn := get_node("btns/permaTriggers/" + str(i))
+		var door := Array(permaDoors[i])
+		if currentBtn.get_overlapping_bodies() != []:
+			if permaButtons[i] == 0:
+				$btns/button.set_cell(permaCells[i], 0, Vector2i(1, 3))
+				$btns/button.set_cell(permaLights[i], 0, Vector2i(1, 1))
+				$door.set_cell(door[0], 0, Vector2i(3, 0))
+				$door.set_cell(door[1], 0, Vector2i(3, 1))
 				permaButtons[i] = 1
 				await get_tree().create_timer(0.5).timeout
 				$btns/button.set_cell(permaCells[i], 0, Vector2i(2, 3))
