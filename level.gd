6d402ef1a4b5790726840335398b239850a3f3ea
Takashi <takashi.salv@proton.me>
2025-07-24 18:42:24 -0700

    Good, a private repo works

--- level.gd
+++ level.gd
@@ -9,4 +9,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if $"button/permaTriggers/1".body_entered():
-		gotoheck
+		
