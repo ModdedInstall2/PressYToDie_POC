09205e7e6ed423dd0583ba504ace060d8025e6fa
Takashi <takashi.salv@proton.me>
2025-07-25 12:11:06 -0700

    staged

--- level.gd
+++ level.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if $"btns/permaTriggers/1".body_entered:
+	if $"btns/permaTriggers/1".get_overlapping_bodies() != []:
 		if btn1 == false:
 			$btns/button.set_cell(Vector2i(12, 6), 0, Vector2i(1, 3))
 			btn1 = true
