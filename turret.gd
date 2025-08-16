1ab77c7007edf2647a1d53e53c00f2c6c0750b56
Takashi <takashi.salv@proton.me>
2025-08-15 17:23:25 -0700

    A bit of work on turrets (it all sucked tho)

--- turret.gd
+++ turret.gd
@@ -12,4 +12,13 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.play(&"dead")
 	
 	move_and_slide()
-	print($Area2D2.get_overlapping_areas())
+	#print($Area2D2.get_overlapping_areas())
+
+func _on_vision_cone_area_body_entered(body: Node2D) -> void:
+	print(body)
+	if body.name == "TurretBody":
+		if (target == 0 and body.get_parent().dead == false) or (target == 1 and body.get_parent().dead == true):
+			$beam.visible = true
+
+func _on_vision_cone_area_body_exited(body: Node2D) -> void:
+	$beam.visible = false
